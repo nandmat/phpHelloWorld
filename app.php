4e4311bbe56ab0e263eb9e7a228ee2ec41274a7b
Nanderson Matheus Reis <87582433+nandmat@users.noreply.github.com>
2022-09-20 15:58:17 -0300

    Add files via upload

---
+++ app.php
@@ -0,0 +1,15 @@
+<?php
+
+$texto = "<h1>Hello, World!</h1>";
+$h2 = "<h2>Hello, World!</h2>";
+$h3 = "<h3>Hello, World!</h3>";
+$h4 = "<h4>Hello, Wold!</h4>";
+$h5 = "<h5>Hello, World</h5>";
+
+echo $texto;
+echo $h2;
+echo $h3;
+echo $h4;
+echo $h5;
+
+?>
